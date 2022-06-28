7cc47c8467d6891e20db9a5040c4509f43f8d529
Thais Rosa <tsrosa18@gmail.com>
2022-06-28 12:25:26 -0300

    atualização desafio8

--- src/challenges.js
+++ src/challenges.js
@@ -34,9 +34,24 @@ function catAndMouse() {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+
+function fizzBuzz(array) {
+  let result = [];
+  for ( i=0; i<array.length; i +=1) {
+   if (array[i] % 3 === 0 && array[i] % 5 === 0 ) {
+   result.push("fizzBuzz");
+ } else if (array[i] % 3 === 0) {
+   result.push("fizz");
+  } else if (array[i] % 5 === 0) {
+   result.push("buzz");
+  } else {
+   result.push("bug!");
+  }
+ }
+ return result;
+ }
+ console.log(fizzBuzz([9, 25]));
+
 
 // Desafio 9
 function encode() {
