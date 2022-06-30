b5ac5fcc5d8ced69802f215c7bbc1a45c9cb3ed7
Thais Rosa <tsrosa18@gmail.com>
2022-06-30 20:04:09 -0300

    desafio 4 erro

--- src/challenges.js
+++ src/challenges.js
@@ -26,22 +26,7 @@ function splitSentence() {
 //   // seu código aqui
 }
 
-// Desafio 4
-
-// 4 - Crie uma função que use concatenação de strings
-// Implemente a função concatName que recebe um array de strings e retorna o último item na primeira posição
-
-// A função `concatName` recebe um array de strings e deve retornar uma string com o formato `'ÚLTIMO ITEM, PRIMEIRO ITEM`, independente do tamanho do array.
-// Exemplo:
-
-// Caso o parâmetro passado para a função concatName seja o array ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'], a função deverá retornar Paolillo, Lucas.
-// O que será testado:
-
-// Retorne 'Paolillo, Lucas' quando o parâmetro passado na funcão concatName seja ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
-
-// Retorne 'ré, foguete' quando o parâmetro passado na funcão concatName seja ['foguete', 'não', 'tem', 'ré'];
-
-// Retorne 'captain, captain' quando o parâmetro passado na funcão concatName seja ['captain', 'my', 'captain'].
+// Desafio 4 - CONCLUIDO
 
 function concatName(arrayStrings) {
   let newArray = []
